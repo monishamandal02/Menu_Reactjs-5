a090b1bf3dfea70638658026449bfc1259c34703
monishamandal02 <monishamandal0203@gmail.com>
2021-05-30 18:08:14 +0530

    Updates

---
+++ precache-manifest.18baa185dc307af05884dcb940bf79b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12b2e701ee4c5eb550605275121cee8",
+    "url": "/Menu_Reactjs-5/index.html"
+  },
+  {
+    "revision": "e6cf13fd7efbcb760f89",
+    "url": "/Menu_Reactjs-5/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/Menu_Reactjs-5/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Menu_Reactjs-5/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6cf13fd7efbcb760f89",
+    "url": "/Menu_Reactjs-5/static/js/main.8b31f644.chunk.js"
+  },
+  {
+    "revision": "f2152546c9d9096cbaf3",
+    "url": "/Menu_Reactjs-5/static/js/runtime-main.0683cb6e.js"
+  }
+]);
